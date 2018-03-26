a16119a367a97a16318c13962ca772c951d99ecd
srfrnk <srfrnk@gmail.com>
2018-03-26 19:24:49 +0300

    fix: more flow

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const davJS = require('../../dav-js');
+const davJS = require('@davfoundation/dav-js');
 const DroneAPI = require('./drone-api');
 const geolib = require('geolib');
 const getElevations = require('./elevation');
@@ -7,20 +7,23 @@ const DRONE_AVG_VELOCITY = 10.0; // m/s
 const DRONE_PRICE_RATE = 10 / 1000; // DAV/m
 const DRONE_CRUISE_ALT = 1000;
 
+const pt1 = { lat: 47.397669, lon: 8.5444809 };
+const pt2 = { lat: 47.3982004, lon: 8.5448531 };
+
 process.env['MISSION_CONTROL_URL'] = 'http://localhost:8888';
 process.env['NOTIFICATION_URL'] = 'https://9991eaca.ngrok.io'; // I used ngrok to point this to localhost:7000, I was having issues making requests to localhost from docker
 
 async function init_sitl() {
-  let droneApi = new DroneAPI();
-  let drones = await droneApi.listDrones();
-  let sitl = drones.find(drone => drone.description.match(/\bSITL\b/));
+  const droneApi = new DroneAPI();
+  const drones = await droneApi.listDrones();
+  const sitl = drones.find(drone => drone.description.match(/\bSITL\b/));
   let state = await droneApi.getState(sitl.id);
 
   console.log(state);
 
-  let dav = new davJS('12345');
+  const dav = new davJS('12345');
 
-  let droneDelivery = dav.needs().forType('drone_delivery', {
+  const droneDelivery = dav.needs().forType('drone_delivery', {
     longitude: state.location.lon,
     latitude: state.location.lat,
     radius: 10e10,
@@ -46,28 +49,33 @@ async function init_sitl() {
   );
 
   function onNeed(need) {
-    let distToPickup = geolib.getDistance(
+    const distToPickup = geolib.getDistance(
       { latitude: state.location.lat, longitude: state.location.lon },
-      { latitude: need.pickup_latitude, longitude: need.pickup_longitude },
+      // { latitude: need.pickup_latitude, longitude: need.pickup_longitude },
+      { latitude: pt1.lat, longitude: pt1.lon },
       1, 1
     );
 
-    let distToDropoff = geolib.getDistance(
-      { latitude: need.pickup_latitude, longitude: need.pickup_longitude },
-      { latitude: need.dropoff_latitude, longitude: need.dropoff_longitude },
+    const distToDropoff = geolib.getDistance(
+      // { latitude: need.pickup_latitude, longitude: need.pickup_longitude },
+      { latitude: pt1.lat, longitude: pt1.lon },
+      // { latitude: need.dropoff_latitude, longitude: need.dropoff_longitude },
+      { latitude: pt2.lat, longitude: pt2.lon },
       1, 1
     );
 
-    let totalDist = distToPickup + distToDropoff;
+    const totalDist = distToPickup + distToDropoff;
 
-    let bid = dav.bid().forNeed(need.id, {
+    const bidInfo = {
       price: `${totalDist / DRONE_PRICE_RATE}`,
       price_type: 'flat',
       price_description: 'Flat fee',
-      time_to_pickup: (distToPickup / DRONE_AVG_VELOCITY)+1,
-      time_to_dropoff: (distToDropoff / DRONE_AVG_VELOCITY)+1,
+      time_to_pickup: (distToPickup / DRONE_AVG_VELOCITY) + 1,
+      time_to_dropoff: (distToDropoff / DRONE_AVG_VELOCITY) + 1,
       ttl: 120 // TTL in seconds
-    });
+    };
+
+    const bid = dav.bid().forNeed(need.id, bidInfo);
     bid.subscribe(
       () => onBidAccepted(bid),
       () => console.log('Bid completed'),
@@ -77,18 +85,18 @@ async function init_sitl() {
 
   function onBidAccepted(bid) {
     console.log(bid);
-    let contract = dav.contract().forBid(bid.id, {
+    const contract = dav.contract().forBid(bid.id, {
       id: '0x98782738712387623876', // Ethereum Smart Contract
       ttl: 120 // TTL in seconds
     });
     contract.subscribe(
-      (contractUpdate) => onContractUpdated(contract,contractUpdate),
+      (contractUpdate) => onContractUpdated(contract, contractUpdate),
       () => console.log('Contract completed'),
       err => console.log(err)
     );
   }
 
-  function onContractUpdated(contract,contractUpdate) {
+  function onContractUpdated(contract, contractUpdate) {
     console.log(contract);
     switch (contractUpdate.state) {
       case 'signed':
@@ -101,47 +109,46 @@ async function init_sitl() {
   }
 
   function beginMission(contract) {
-    let mission = dav.mission().begin(contract.id, {
+    const mission = dav.mission().begin(contract.id, {
       id: '0x98782738712387623876', // Etherum Smart Contract
       ttl: 120 // TTL in seconds
     });
     mission.subscribe(
-      (missionUpdate) => onMissionUpdated(mission,missionUpdate),
+      (missionUpdate) => onMissionUpdated(mission, missionUpdate),
       () => console.log('Mission completed'),
       err => console.log(err)
     );
   }
 
-  async function onMissionUpdated(mission,missionUpdate) {
-    let drone = {/* This is the drone API - not part of DAV-JS SDK */ };
-    let [pickupAlt, dropoffAlt] = await getElevations([mission.pickup, mission.dropoff]);
+  async function onMissionUpdated(mission, missionUpdate) {
+    const [pickupAlt, dropoffAlt] = await getElevations([mission.pickup, mission.dropoff]);
     switch (missionUpdate.state) {
       case 'started':
-        drone.goto(sitl.id,
+        droneApi.goto(sitl.id,
           mission.pickup.lat, mission.pickup.lng,
           DRONE_CRUISE_ALT - state.location.alt, pickupAlt - state.location.alt
           , false)
           .then((pos) => {
             mission.update({ state: 'atPickup', lat: pos.lat, lng: pos.lng });
           });
-        drone.updatesPosition.then(pos => {
+        droneApi.updatesPosition.then(pos => {
           mission.update({ state: 'movingToPickup', lat: pos.lat, lng: pos.lng });
         });
         break;
       case 'packageReady':
-        drone.goto(sitl.id,
+        droneApi.goto(sitl.id,
           mission.dropoff.lat, mission.dropoff.lng,
           DRONE_CRUISE_ALT - state.location.alt, dropoffAlt - state.location.alt
           , true)
           .then((pos) => {
             mission.update({ state: 'atDropoff', lat: pos.lat, lng: pos.lng });
           });
-        drone.updatesPosition.then(pos => {
+        droneApi.updatesPosition.then(pos => {
           mission.update({ state: 'movingToDropoff', lat: pos.lat, lng: pos.lng });
         });
         break;
       case 'done':
-        drone.standBy();
+        droneApi.standBy();
         break;
     }
   }
